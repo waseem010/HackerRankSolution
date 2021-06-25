d2aefa4784b33a673758878d6deea77085b62195
waseem010 <71478157+waseem010@users.noreply.github.com>
2021-06-25 23:19:48 +0530

    Add files via upload

---
+++ chocalateFeast.cpp
@@ -0,0 +1,46 @@
+		#include<bits/stdc++.h>
+		using namespace std;
+
+		int main(){
+
+			int t;
+			cin>>t;
+
+			while(t--){
+
+
+				int n,c,m;
+
+				cin>>n>>c>>m;
+
+
+				
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			}
+
+
+
+		return 0;
+		}
